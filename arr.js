66f63175d510ab6d06888ac5e1e2473beba08bba
Sandhra M <sandhram9736@gmail.com>
2026-05-07 15:53:38 +0530

    first commit

---
+++ arr.js
@@ -0,0 +1,10 @@
+let numbers = [10,20,30,40,50]
+console.log(numbers[1])
+let nums = [60,70,80,90,99]
+console.log(nums[nums.length-1]);
+let students = ["sangi","nanda","san"];
+students.push("sam");
+console.log(students);
+students.pop("sangi");
+console.log(students);
+console.log(students.length);
